51972ec7b4b04ede09e170adab960266bef303fd
ynFatal <63413763@qq.com>
2018-09-25 12:39:11 +0800

    chapter2 测试了复杂数据封装

--- chapter2/src/main/java/com/fatal/config/Fatal6.java
+++ chapter2/src/main/java/com/fatal/config/Fatal6.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -15,29 +14,13 @@ import java.util.Set;
  * @date: 2018/9/20 0020 16:16
  */
 @Component
-@ConfigurationProperties(prefix = "fatal")
+@ConfigurationProperties(prefix = "fatal6")
 @ToString
 @Data
-public class Fatal2 {
+public class Fatal6 {
 
     private String name;
 
     private Integer age;
-
-    /**
-     * 测试对象、Map
-     */
-    private Fatal6 fatal6;
-
-    private Map<String, Integer> map;
-
-    /**
-     * 测试集合、List，Set 数组数据封装
-     */
-    private String[] strings;
-
-    private List<String> list;
-
-    private Set<String> set;
 
 }

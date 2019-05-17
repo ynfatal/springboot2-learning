6066377ee9089c4c7486fec61e175478a4fea6d2
ynFatal <63413763@qq.com>
2019-05-17 18:03:41 +0800

    修改一点东西

--- chapter1/src/test/java/com/fatal/Chapter1ApplicationTests.java
+++ chapter1/src/test/java/com/fatal/Chapter1ApplicationTests.java
@@ -16,7 +16,7 @@ import java.net.URL;
 /**
  * WebEnvironment: web环境
  *  1.MOCK: 提供一个Mock的Servlet环境，内置的Servlet容器并没有真实的启动，主要搭配使用@AutoConfigureMockMvc
-*   2.RANDOM_PORT: 提供一个真实的Servlet环境，也就是说会启动内置容器，然后使用的是随机端口
+ *  2.RANDOM_PORT: 提供一个真实的Servlet环境，也就是说会启动内置容器，然后使用的是随机端口
  *  3.DEFINED_PORT: 这个配置也是提供一个真实的Servlet环境，使用的默认的端口，如果没有配置就是8080
  *  4.NONE: 这是个神奇的配置，跟Mock一样也不提供真实的Servlet环境
  */
@@ -25,7 +25,7 @@ import java.net.URL;
 public class Chapter1ApplicationTests {
 
     /**
-     * 必须提供真实的Servlet环境，RANDOM_PORT 或者 DEFINED_PORT
+     * @LocalServerPort 必须提供真实的Servlet环境，RANDOM_PORT 或者 DEFINED_PORT
      */
     @LocalServerPort
     private int port;

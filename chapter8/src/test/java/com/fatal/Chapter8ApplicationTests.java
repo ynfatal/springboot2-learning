d303fdde53881f6f03fe650440614531cc916431
ynFatal <63413763@qq.com>
2019-06-14 19:06:09 +0800

    application.yml 配置了type-aliases-package: com.fatal.entity 后， `写全类名`或者`只写类名`都能找到对应的实体 例如： 没配置type-aliases-package的写法：com.fatal.entity.User 配置了type-aliases-package后支持的写法：com.fatal.entity.User 或 User

--- chapter8/src/test/java/com/fatal/Chapter8ApplicationTests.java
+++ chapter8/src/test/java/com/fatal/Chapter8ApplicationTests.java
@@ -2,6 +2,8 @@ package com.fatal;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.boot.autoconfigure.MybatisProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Chapter8ApplicationTests {
 
+    @Autowired
+    private MybatisProperties mybatisProperties;
+
     @Test
     public void contextLoads() {
+        System.out.println("!!!");
     }
 
 }

7841240591bfa66fa3515ce3ce4cc57de9dd5179
ynFatal <63413763@qq.com>
2019-09-22 22:46:12 +0800

    添加注释

--- chapter32/src/main/java/com/fatal/Chapter32Application.java
+++ chapter32/src/main/java/com/fatal/Chapter32Application.java
@@ -3,6 +3,10 @@ package com.fatal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 2.1.6 开始不需要自己加 @EnableElasticsearchRepositories
+ * springboot 已经在 ElasticsearchRepositoriesRegistrar 中帮我们加上了
+ */
 @SpringBootApplication
 public class Chapter32Application {
 
